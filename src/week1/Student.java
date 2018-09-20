913caac2a6cb18eebd7fcdb9520d340e3218efeb
lytamay <43265798+lytamay@users.noreply.github.com>
2018-09-21 04:47:14 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,105 +1,70 @@
 package week1;
 
 public class Student {
+	private String name , id, group, email;
 
-    private String name , id,group, email; // TODO: khai báo các thuộc tính cho Student
+	public String getName() {
+		return name;
+	}
 
-    // TODO: khai báo các phương thức getter, setter cho Student
-    public String getName (String n){
-        return n;
-    }
-    public void setName(String n){
-        name=n
-    }
-    public void setId(String id){
-        id=id;
-    }
-    public String getId(String id){
-        return id;   
-    }
-    public void setId(String Group ){
-        group=Group;
-    }
-    public String getId(String Group){
-        return  Group;
-    }
-    public void setId(String Email){
-        email=Email;
-    }
-    public String getId(String Email){
-        return Email;   
-    }
-    public Student(){
-         name = “Student”; id=“000”; group=“INT22041”; email=”uet@vnu.edu.vn”;
-    }
-    public Student(String n, String sid, String em){
-        name =n;
-        id=sid;
-        email=em;
-         group= “INT22041”
-    }
-    public Student(Student s)
-    {
-        name=s.name;
-        id=s.id;
-        group=s.group;
-        email=s.email;
-    }
-    /**
-    Student() {
-        // TODO:
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    Student(String n, String sid, String em) {
-        // TODO:
-    }
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * Constructor 3
-     * @param s
-     */
-    Student(Student s) {
-        // TODO:
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    String getInfo() {
-        Student studen= Student();
-        System.out.println("TenSV: "+studen.name);
-        System.out.println("ma so sv: "+studen.id);
-        System.out.println("lop: "+studen.group);
-        System.out.println("Email: "+studen.email);   
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
-    }
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getInfo() {
+		System.out.println("ten sv : "+name);
+		System.out.println("ma sv : "+id );
+		System.out.println("lop : "+group);
+		System.out.println("email sv : "+ email);
+		return null;
+	}
+	public Student(){
+		name="Student";
+		id="000";
+		group="INT22041";
+		email="uet@vnu.edu.vn";
+	}
+	public Student(String n, String sid, String em) {
+		name=n;
+		id=sid;
+		email=em;
+		group="INT22041";
+	}
+	public Student(String n, String id, String group, String email) {
+		this.name=n;
+		this.id=id;
+		this.group=group;
+		this.email=email;
+	}
+	public Student(Student s){
+		this.name=s.name;
+		this.id=s.id;
+		this.group=s.group;
+		this.email=s.email;
+	}
+
 }
-public class  StudentManagement(){
-    public static void main(String[] args){
-        Student student1= new Student();
-        student1.setName("ly ta may");
-        student1.setId("17020887");
-        student1.setGroup("IE");
-        Student student2= new Student();
-        student2.Student("nguyen qunag thang","56121","quangthang.com");
-        student1.setEmail("lytamay@gmail.com");
-        System.out.println("Ten sv la: "+ student1.getName());
-        System.out.println("Thong tin sv la : "+student1.getInfo());
-        System.out.println("thong tin sv : "+student1.Student());
-         System.out.println("thong tin sv : "+student1.Student("ho thi dung","1451142124"),"hothidung.com");
-        System.out.println("thong tin sv a: "+student1.Student(student2));
-        public boolean sameGroup(Student s1,Student s2){
-           if(student1.getGroup()==student2.getGroup()}
-              {
-                  System.out.println("cung lop");
-              }
-              else System.out.println("Khong cung lop");
-        }
-         Student student2= new Student();
-         Student student2= new Student();
-         Student student2= new Student();
-          
