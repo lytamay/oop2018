5adf95f0b8b31b6adfef6b7b8036bcdaa3ade78d
lytamay <43265798+lytamay@users.noreply.github.com>
2018-10-31 15:13:16 +0700

    bai1

---
+++ Division.java
@@ -0,0 +1,33 @@
+package cau1;
+
+public class Division  extends BynaryExpression {
+	Expression left;
+	Expression right;
+	
+	public Division(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	@Override
+	Expression left() {
+		
+		return this.left;
+	}
+
+	@Override
+	Expression right() {
+		
+		return this.right;
+	}
+
+	@Override
+	public String toString() {
+		return left.evaluate() + "/ "+ right.evaluate();
+	}
+
+	@Override
+	public int evaluate() {
+		return left.evaluate()/right.evaluate();
+	}
+}
